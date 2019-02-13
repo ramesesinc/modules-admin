c5621be7cf32b1b9c207d29bd45b5e93beb47633
worgflores <worgflores@gmail.com>
2019-02-13 18:15:55 +0800

    added

---
+++ client/rameses-enterprise-admin/src/com/rameses/admin/models/UserRoleLookupModel.groovy
@@ -0,0 +1,36 @@
+package com.rameses.admin.models;
+
+import com.rameses.rcp.annotations.*;
+import com.rameses.rcp.common.*;
+import com.rameses.seti2.models.*;
+import com.rameses.osiris2.common.*;
+import com.rameses.util.*;
+import com.rameses.osiris2.common.*;
+import com.rameses.rcp.common.*;
+import com.rameses.osiris2.client.*;
+import com.rameses.enterprise.models.*;
+
+public class UserRoleLookupModel extends CrudLookupModel {
+        
+    public def getCustomFilter() {
+        def f = [];
+        def p = [:];
+        /*
+        //do not add orgid because somehow it is being injected somewhere. 
+        if( query.orgid ) {
+            f << "orgid=:orgid";
+            p.orgid = query.orgid;
+        }
+        */
+        if( query.domain ) {
+            f << "domain=:domain";
+            p.domain = query.domain;
+        }
+        if(query.role) {
+            f << "role=:role";
+            p.role = query.role;
+        }
+        if(!f) return null;
+        return [ f.join(" AND "), p ]; 
+    }
+}
